38352a885cee2f2238256dd00fbb7192d864bf60
ZeroZero19 <34528636+ZeroZero19@users.noreply.github.com>
2022-09-16 11:20:17 +0700

    fix name bug

--- server.js
+++ server.js
@@ -125,7 +125,6 @@ app.post('/api/register', async (req, res) => {
 	res.json({ status: 'ok' })
 })
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number, () => {
+app.listen(process.env.PORT || 9999, () => {
 	console.log('Server up at')
 })
